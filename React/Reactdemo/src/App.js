1ee524a3a22edb8f2a4c3dc71d69149c85c313e8
PootisHunter <98407040+PootisHunter@users.noreply.github.com>
2023-04-24 15:30:04 +0300

    Add files via upload

---
+++ React/Reactdemo/src/App.js
@@ -0,0 +1,24 @@
+
+import "./Components/body.css"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+      <div className="body">
+      </div>
+      <div className="container">
+        <h1>Grand Theft Auto 6</h1>
+      </div>
+      <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </>
+  );
+}
+
+export default App;
